e7014fbf5d4c4c8e3ca5fc2e2b162e7bbd3a09fc
Dayong Wang <wuyoutech@gmail.com>
2025-01-13 13:15:26 +0800

    feature: add status machine for feeder

--- src/Application/feeder.c
+++ src/Application/feeder.c
@@ -33,20 +33,20 @@ void feeder_config(void) {
     // config feeder motor control pin
     rcu_periph_clock_enable(RCU_GPIOB);
     gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_14);
-    gpio_bit_set(GPIOB, GPIO_PIN_14); // Enable motor driver
+    gpio_bit_set(GPIOB, GPIO_PIN_14);  // Enable motor driver
 
     rcu_periph_clock_enable(RCU_GPIOC);
-    gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_6); // motor1
+    gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_6);  // motor1
     gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_7);
 
-    gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_8); // motor2
+    gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_8);  // motor2
     gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_9);
 
     rcu_periph_clock_enable(RCU_GPIOA);
-    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_8); // motor3
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_8);  // motor3
     gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_9);
 
-    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_10); // motor4
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_10);  // motor4
     gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_11);
 }
 
@@ -102,14 +102,47 @@ void feeder4_set_idle(void) {
     gpio_bit_reset(GPIOA, GPIO_PIN_11);
 }
 
-
 uint16_t adc_channel_sample(uint8_t channel) {
     adc_regular_channel_config(ADC0, 0U, channel, ADC_SAMPLETIME_71POINT5);
     adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
 
-    while (!adc_flag_get(ADC0, ADC_FLAG_EOC))
-        ;
+    while (!adc_flag_get(ADC0, ADC_FLAG_EOC));
     adc_flag_clear(ADC0, ADC_FLAG_EOC);
 
     return adc_regular_data_read(ADC0);
+}
+
+enum feeder_status { feeder_status_idle, feeder_status_feed, feeder_status_reverse_to_release, feeder_status_ready };
+
+int feeder1_status = feeder_status_idle;
+void feeder1_task(void) {
+    switch (feeder1_status) {
+        case feeder_status_idle:
+            // TODO: move adc_channel_sample(5) > 1000 to a function
+            if (adc_channel_sample(5) > 1000) {
+                feeder1_set_forward();
+                feeder1_status = feeder_status_feed;
+            }
+            break;
+        case feeder_status_feed:
+            feeder1_set_forward();
+            // TODO: identify mux module hall sensor status
+
+            if (1) {
+                feeder1_set_idle();
+                feeder1_status = feeder_status_reverse_to_release;
+            }
+        case feeder_status_reverse_to_release:
+            feeder1_set_backward();
+            if (1) {
+                feeder1_set_idle();
+                feeder1_status = feeder_status_ready;
+            }
+            break;
+        case feeder_status_ready:
+            if (1) {
+                feeder_status_idle;
+            }
+            break;
+    }
 }
--- src/Application/vision_uart.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void vision_uart_init(void);
-void vision_uart_process(void);
